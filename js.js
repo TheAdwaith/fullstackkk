f0727bf8b021fc5b5335f43ea9ade32f7846b280
TheAdwaith <151132727+TheAdwaith@users.noreply.github.com>
2026-01-26 17:06:15 +0530

    Create js.js

---
+++ js.js
@@ -0,0 +1,6 @@
+let username;
+
+document.getElementById("button").onclick = function(){
+    username = getElementById("input").value;
+    document.getElementById("MyText").textContent = username;
+}

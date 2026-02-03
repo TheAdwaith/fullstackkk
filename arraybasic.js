86f6b84c164422d24acad6a59dcbb872f51d558b
Adwaith <rollo12k@gmail.com>
2026-02-03 13:06:32 +0530

    Update arraybasic.js

--- arraybasic.js
+++ arraybasic.js
@@ -7,3 +7,12 @@ fruits.forEach(display);
 function display(element){
     console.log(element);
 }
+
+let dates = ["2024-4-23" , "2023-12-16" , "2020-7-9"]
+
+const newDate = dates.map(newdates)
+
+function newdates(element){
+    date = element.split("-");
+    console.log(`${date[2]}-${date[1]}-${date[0]}`);
+}
